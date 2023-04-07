fc3fcda228ae38eec960919e02c9bd26f2c1df73
wymansen67 <DenisGorbatuk77@gmail.com>
2023-04-07 15:46:14 +0500

    Initial commit

---
+++ app/src/main/java/com/example/prakt16/CalendarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.prakt16
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CalendarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+    }
+}
---
+++ app/src/main/java/com/example/prakt16/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.prakt16
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
